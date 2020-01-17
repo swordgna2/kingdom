3febfeddc96d3c809c312563b4b30dc83fbd4db2
Polishuk <polishukgennadij@mail.ru>
2020-01-17 09:57:38 +0300

    fix: переработана обработка значений панели статистики

--- src/components/StatisticsPanel.vue
+++ src/components/StatisticsPanel.vue
@@ -2,12 +2,12 @@
     <div class="statistics-container" v-if="myKingdom.year">
         <span>Статистика</span>
         <div class="group" v-for="(group, groupIndex) in statisticsScheme" :key="groupIndex">
-            <div class="group-caption"><font-awesome-icon icon="plus" v-if="group.expanded"/><font-awesome-icon icon="minus" v-else/>&nbsp;{{ group.caption }}</div>
+            <div class="group-caption"><font-awesome-icon icon="plus" v-if="group.collapsed"/><font-awesome-icon icon="minus" v-else/>&nbsp;{{ group.caption }}</div>
             <div class="group-data">
                 <div class="list-row" v-for="(item, itemIndex) in group.sub" :key="itemIndex">
-                    <div class="list-caption">{{ typeof item === 'object' ? item.caption : item }}</div>
-                    <div class="list-value">{{ myKingdom[groupIndex][itemIndex] }}</div>
-                    <div class="list-dimension">{{ ( typeof item === 'object' ? item.dimension : '' ) || group.dimension }}</div>
+                    <div class="list-caption">{{ getItemName(item) }}</div>
+                    <div class="list-value">{{ translateValueWithItemSettings(groupIndex, itemIndex, item) }}</div>
+                    <div class="list-dimension">{{ getItemDimension(item, group) }}</div>
                 </div>
             </div>
         </div>
@@ -30,6 +30,19 @@
         },
         created () {
             console.log(this.myKingdom);
+        },
+        methods: {
+            getItemName (item) {
+                return typeof item === 'object' ? item.caption : item;
+            },
+            translateValueWithItemSettings (groupIndex, itemIndex, itemParameters) {
+                const returnValue = this.myKingdom[groupIndex][itemIndex] + '!'; // value + '!';
+                console.log(itemParameters, groupIndex, itemIndex, returnValue);
+                return returnValue;
+            },
+            getItemDimension (item, group) {
+                return (typeof item === 'object' ? item.dimension : '') || group.dimension;
+            }
         }
     };
 </script>
